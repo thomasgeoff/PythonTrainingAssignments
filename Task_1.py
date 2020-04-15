9e5f15349f742d96b09a4f440c685af90faf2743
thomasgeoff <thomasgeoff01@gmail.com>
2020-04-15 00:10:44 -0700

    Update Task_1.py
    Task One updated

--- Task_1.py
+++ Task_1.py
@@ -1,3 +1,4 @@
+#TASK ONE: NUMBERS AND VARIABLES
 ''' 1.Create three variables in a single line and assign different values to them and make sure their 
  data types are different. Like one is int, another one is float and the last one is a string.'''
 integer,string,floatvar = 1,"Geoff",4.12
